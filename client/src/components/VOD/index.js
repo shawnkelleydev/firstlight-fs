ee969671f9d54453e4c6f70a5d8e01b3091a0068
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2022-01-19 14:40:55 -0700

    vod refactor / read chapter

--- client/src/components/VOD/index.js
+++ client/src/components/VOD/index.js
@@ -1,7 +1,8 @@
 //dependencies
 import Interweave from "interweave";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import "./styles.css";
 
 //component
 export default function VOD(props) {
@@ -9,6 +10,7 @@ export default function VOD(props) {
   const [VOD, setVOD] = useState(null);
   const [citation, setCitation] = useState(null);
   const [query, setQuery] = useState(null);
+  const [chapter, setChapter] = useState(null);
   const esv = process.env.REACT_APP_ESVAPI;
   const params = useParams();
 
@@ -30,6 +32,8 @@ export default function VOD(props) {
         .then((res) => res.json())
         .then((data) => {
           setCitation(data.canonical);
+          let chap = data.canonical.split(":")[0];
+          setChapter(chap);
           let pass = data.passages[0];
           setVOD(pass);
         });
@@ -52,10 +56,13 @@ export default function VOD(props) {
           props.getVerse();
         }}
       >
-        get another verse &rarr;
+        get another verse &#187;
       </button>
       <Interweave content={VOD} />
       <span className="vod-cit">{citation} ESV</span>
+      <Link to={`/bible/${chapter}`} className="get-verse">
+        read {chapter} &#187;
+      </Link>
     </div>
   );
 }
