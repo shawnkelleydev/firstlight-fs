00152eea2b9eb250a8957ea752a04ba3089a6aab
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2022-03-01 10:41:07 -0700

    mobile warning

--- client/src/components/Tasks/index.js
+++ client/src/components/Tasks/index.js
@@ -6,7 +6,8 @@ export default function Tasks() {
   return (
     <div className="Tasks">
       <p style={{ color: "yellow", paddingBottom: "16px" }}>
-        ** under construction **
+        ** under construction ** <br />
+        doesn't work on mobile
       </p>
       {params.layout ? <Outlet /> : <h1>Please select a layout.</h1>}
     </div>
